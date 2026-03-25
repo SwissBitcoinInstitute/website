084ae5337446f02e4763508ddc815d036ebaeca7
Dominic Huber <huber.dominic@proton.me>
2026-03-25 21:52:17 +0100

    remove-get-started-button

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -113,9 +113,6 @@ const Header = () => {
           {/* CTA Buttons & Search */}
           <div className="hidden md:flex items-center space-x-3">
             <SiteSearch />
-            <Button variant="outline" size="sm" asChild>
-              <Link href="/inquiry?service=research&discovery=true#service-selection">Get Started</Link>
-            </Button>
             <Button variant="default" size="sm" asChild>
               <Link href="/contact">Contact</Link>
             </Button>
@@ -183,9 +180,6 @@ const Header = () => {
                 <div className="flex justify-center pb-2">
                   <SiteSearch />
                 </div>
-                <Button variant="outline" size="sm" className="w-full" asChild>
-                  <Link href="/inquiry?service=research&discovery=true#service-selection">Get Started</Link>
-                </Button>
                 <Button variant="default" size="sm" className="w-full" asChild>
                   <Link href="/contact">Contact</Link>
                 </Button>
